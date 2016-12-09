512cff5034bf6f434b8666bc59abbf4993115962
ancalled <ankolt@gmail.com>
2016-12-09 12:45:41 +0600

    Library added

--- main.cpp
+++ main.cpp
@@ -2,23 +2,24 @@
 #include <cstring>
 #include "tablesynth.h"
 #include "YinDecoder.h"
+#include "synthesizer.h"
 
 using namespace std;
 
 int main() {
 
-    uint32_t sampleRate = 19200;  //atmega adc prescale = 64
+//    uint32_t sampleRate = 19200;  //atmega adc prescale = 64
 //    uint32_t sampleRate = 38400;  //atmega adc prescale = 32
 //    uint32_t sampleRate = 44100;
-//    uint32_t sampleRate = 62500;
+    uint32_t sampleRate = 62500;
 
-//    const char *mes = "hjntdb982ilj6etj6e3l\0";
-    const char *mes = "01581359815183159913\0";
+    const char *mes = "hjntdb982ilj6etj6e3l\0";
+//    const char *mes = "01581359815183159913\0";
 
-    tablesynth synth(sampleRate);
+    synthesizer synth(sampleRate);
 
     uint32_t size = (uint32_t) (strlen(mes) * (sampleRate * (TOP_TIME + RAMP_TIME) / 1000)) + 1;
-    uint8_t samples[size];
+    int8_t samples[size];
 //    uint8_t* samples;
 
     uint32_t gen = synth.generate(samples, size, mes);
---
+++ synthesizer.cpp
@@ -0,0 +1,77 @@
+//
+// Created by ancalled on 12/9/16.
+//
+
+#include <cstddef>
+#include <cstring>
+#include <cmath>
+#include "synthesizer.h"
+
+synthesizer::synthesizer(uint32_t sampleRate) : sampleRate(sampleRate) {
+    frame = 0;
+    argument = 0;
+    rampSamples = (uint16_t) ((sampleRate * RAMP_TIME) / 1000);
+    topSamples = (uint16_t) ((sampleRate * TOP_TIME) / 1000);
+}
+
+
+uint32_t synthesizer::generate(int8_t *samples, uint32_t size, const char *mes) {
+    uint32_t proc = 0;
+    size_t len = strlen(mes);
+    float freq = 0, prevFreq = 0;
+    const uint8_t halfampl = 1 << 5;
+    const uint8_t amplStep = (uint8_t) (rampSamples / halfampl);
+
+    for (int i = 0; i < len; i++) {
+        char ch = mes[i];
+        sound_symbol ss = findSymbol(ch);
+        if (ss.symbol != '\0') {
+            if (!freq) {
+                freq = ss.freq;
+            }
+            if (!prevFreq) {
+                prevFreq = freq;
+            }
+            float freqGradient = (ss.freq - prevFreq) / rampSamples;
+            uint8_t amplitude = halfampl;
+
+            for (int j = 0; j < topSamples + rampSamples; j++) {
+                double arg = (2 * PI * (argument++) * freq) / sampleRate;
+//                if (argument >= sampleRate) {
+//                    argument = 0;
+//                }
+//                printf("%d\n", argument);
+//                printf("%.8f\n", arg);
+//                printf("%.8f\n", freq);
+
+                double sn = sin(arg);
+                double smpl = amplitude * sn;
+                int8_t bt = (int8_t) round(smpl);
+                samples[proc++] = bt;
+                if (j < rampSamples) {
+                    freq += freqGradient;
+                    if (!(j % amplStep) && !(amplitude & 0x80)) {
+                        amplitude++;
+                    }
+                }
+
+                if (proc >= size - 1) return proc;
+            }
+
+            frame++;
+            prevFreq = ss.freq;
+        }
+    }
+    return proc;
+}
+
+
+synthesizer::sound_symbol synthesizer::findSymbol(char ch) {
+    for (int i = 0; i < SYMBS; i++) {
+        sound_symbol s = SYMBOLS[i];
+        if (s.symbol == ch) {
+            return s;
+        }
+    }
+    return {'\0', -1, 0};
+}
