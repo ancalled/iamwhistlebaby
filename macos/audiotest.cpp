59918aeb65502f6dc320350c1f6db24774b113c7
Andrey Koltochnik <ankolt@gmail.com>
2017-01-07 02:17:11 +0600

    MacOS synthesizer example implemented

--- macos/audiotest.cpp
+++ macos/audiotest.cpp
@@ -4,83 +4,59 @@
 #include <cstdio>
 #include <iostream>
 #include "AudioToolbox/AudioToolbox.h"
-
-
-typedef struct {
-    double phase, phase_inc;
-    int count;
-} PhaseBlah;
+#include "Synthesizer.h"
 
 
 void callback(void *ptr, AudioQueueRef queue, AudioQueueBufferRef buf_ref) {
-    OSStatus status;
-    PhaseBlah *p = (PhaseBlah *) ptr;
-    AudioQueueBuffer *buf = buf_ref;
-    int nsamp = buf->mAudioDataByteSize / 2;
-    short *samp = (short *) buf->mAudioData;
-    int ii;
-    printf("Callback! nsamp: %d\n", nsamp);
-    for (ii = 0; ii < nsamp; ii++) {
-        samp[ii] = (short) (30000.0 * sin(p->phase));
-        p->phase += p->phase_inc;
-        //printf("phase: %.3f\n", p->phase);
-    }
-    p->count++;
-    status = AudioQueueEnqueueBuffer(queue, buf_ref, 0, NULL);
-    printf("Enqueue status: %d\n", status);
+    printf("Callback!\n");
 }
 
 
 int main(int argc, char *argv[]) {
-    AudioQueueRef queue;
-    PhaseBlah phase = {0, 2 * 3.14159265359 * 450 / 44100};
-    OSStatus status;
-    AudioStreamBasicDescription fmt = {0};
-    AudioQueueBufferRef buf_ref, buf_ref2;
+    int sr = 44100;
+    Synthesizer synth(sr * 2);
 
-    fmt.mSampleRate = 44100;
+    std::string toEncode = "hjntdb982ilj6etj6e3l\0";
+    int16_t samplesPerSoud = (int16_t) (sr * (RAMP_TIME + TOP_TIME) / 1000);
+    uint32_t size = (uint32_t) (toEncode.size() * samplesPerSoud) + 1;
+
+    AudioQueueRef queue;
+    AudioStreamBasicDescription fmt = {0};
+    fmt.mSampleRate = sr;
     fmt.mFormatID = kAudioFormatLinearPCM;
     fmt.mFormatFlags = kAudioFormatFlagIsSignedInteger | kAudioFormatFlagIsPacked;
     fmt.mFramesPerPacket = 1;
-    fmt.mChannelsPerFrame = 1; // 2 for stereo
-    fmt.mBytesPerPacket = fmt.mBytesPerFrame = 2; // x2 for stereo
+    fmt.mChannelsPerFrame = 1;
+    fmt.mBytesPerPacket = fmt.mBytesPerFrame = 2;
     fmt.mBitsPerChannel = 16;
 
-    status = AudioQueueNewOutput(&fmt, callback, &phase, CFRunLoopGetCurrent(),
-                                 kCFRunLoopCommonModes, 0, &queue);
+    AudioQueueBufferRef queueBuffer;
+    OSStatus status = AudioQueueNewOutput(&fmt, callback, NULL, CFRunLoopGetCurrent(),
+                                          kCFRunLoopCommonModes, 0, &queue);
 
-    if (status == kAudioFormatUnsupportedDataFormatError) puts("oops!");
-    else printf("NewOutput status: %d\n", status);
+    if (status == kAudioFormatUnsupportedDataFormatError) {
+        puts("oops!");
+    }
 
-    status = AudioQueueAllocateBuffer(queue, 20000, &buf_ref);
-    printf("Allocate status: %d\n", status);
+    AudioQueueAllocateBuffer(queue, size, &queueBuffer);
 
-    AudioQueueBuffer *buf = buf_ref;
-    printf("buf: %p, data: %p, len: %d\n", buf, buf->mAudioData, buf->mAudioDataByteSize);
-    buf->mAudioDataByteSize = 20000;
+    queueBuffer->mAudioDataByteSize = size;
+    printf("Size: %d\n", queueBuffer->mAudioDataByteSize);
 
-    callback(&phase, queue, buf_ref);
+    int8_t *samp = (int8_t *) queueBuffer->mAudioData;
+    uint32_t gen = synth.generate(samp, size, toEncode.c_str());
+    printf("Generated %d samples.", gen);
 
-    status = AudioQueueAllocateBuffer(queue, 20000, &buf_ref2);
-    printf("Allocate2 status: %d\n", status);
+    AudioQueueEnqueueBuffer(queue, queueBuffer, 0, NULL);
+    AudioQueueSetParameter(queue, kAudioQueueParam_Volume, 1.0);
+    AudioQueueStart(queue, NULL);
 
-    buf = buf_ref2;
-    buf->mAudioDataByteSize = 20000;
+    CFRunLoopRunInMode(
+            kCFRunLoopDefaultMode,
+            5, // seconds
+            false // don't return after source handled
+    );
 
-    callback(&phase, queue, buf_ref2);
-
-    status = AudioQueueSetParameter(queue, kAudioQueueParam_Volume, 1.0);
-    printf("Volume status: %d\n", status);
-
-    status = AudioQueueStart(queue, NULL);
-    printf("Start status: %d\n", status);
-
-    while (phase.count < 15)
-        CFRunLoopRunInMode(
-                kCFRunLoopDefaultMode,
-                0.25, // seconds
-                false // don't return after source handled
-        );
-
-    return 0;
 }
+
+
